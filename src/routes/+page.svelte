0ce33499edfd07e00a701f33532a78646e94efd0
Hodan <hodan@Hodans-MacBook-Air.local>
2024-10-01 23:16:49 +0200

    remove X

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -161,7 +161,7 @@
         <a target="_blank" href="https://www.facebook.com/luma.prod/"><img src="icons/facebook.svg" alt="Facebook" class="w-5 h-5 mx-2"></a>
         <a target="_blank" href="https://www.instagram.com/luma.production_"><img src="icons/instagram.svg" alt="Instagram" class="w-5 h-5 mx-2"></a>
         <a target="_blank" href="https://www.tiktok.com/@luma.producti0n"><img src="icons/tiktok.svg" alt="TikTok" class="w-5 h-5 mx-2"></a>
-        <a target="_blank" href="https://www.google.com/"><img src="icons/twitter-x.svg" alt="Twitter | X" class="w-5 h-5 mx-2"></a>
+        <!-- <a target="_blank" href="https://www.google.com/"><img src="icons/twitter-x.svg" alt="Twitter | X" class="w-5 h-5 mx-2"></a> -->
         <!-- <a target="_blank" href="https://www.google.com/"><img src="icons/linkedin.svg" alt="LinkedIn" class="w-5 h-5 mx-2"></a> -->
     </div>
 </main>
