3aa559b72ad27c423d944c65d605a7d4efaac7bc
Hodan <hodan>
2024-10-19 12:59:24 +0200

    removed tiktok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -176,7 +176,7 @@
             <a target="_blank" href="https://www.youtube.com/@luma.production"><img src="icons/youtube.svg" alt="Youtube" class="w-5 h-5 mx-2 filter invert"></a>
             <a target="_blank" href="https://www.facebook.com/luma.prod/"><img src="icons/facebook.svg" alt="Facebook" class="w-5 h-5 mx-2 filter invert"></a>
             <a target="_blank" href="https://www.instagram.com/luma.production_"><img src="icons/instagram.svg" alt="Instagram" class="w-5 h-5 mx-2 filter invert"></a>
-            <a target="_blank" href="https://www.tiktok.com/@luma.producti0n"><img src="icons/tiktok.svg" alt="TikTok" class="w-5 h-5 mx-2 filter invert"></a>
+            <!-- <a target="_blank" href="https://www.tiktok.com/@luma.producti0n"><img src="icons/tiktok.svg" alt="TikTok" class="w-5 h-5 mx-2 filter invert"></a> -->
             <a target="_blank" href="mailto:hello@luma-production.com"><img src="icons/mail.svg" alt="Mail" class="w-5 h-5 mx-2 filter invert"></a>
             <a target="_blank" href="tel:+420776781248"><img src="icons/phone.svg" alt="Phone" class="w-5 h-5 mx-2 filter invert"></a>
             <!-- <a target="_blank" href="https://www.google.com/"><img src="icons/twitter-x.svg" alt="Twitter | X" class="w-5 h-5 mx-2"></a> -->
