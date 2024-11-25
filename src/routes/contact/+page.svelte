317bf62dfb03ad85268ae5ba365ae8e7545caccd
Hodan <hodan>
2024-11-25 19:40:52 +0100

    fix: fix contact page responsiveness

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,15 +1,15 @@
-<div class="h-[100vh]">
-    <a href="/"><h1 class="h-[10vh] text-center bg-white text-[#1e1e1e] font-['Montserrat'] font-black text-6xl py-7">Luma.</h1></a>
-    <div class="w-full h-[60vh] flex text-center justify-center flex-col font-['Montaga'] text-3xl gap-6 bg-white text-[#1e1e1e]">
-        <p class="text-4xl">Write a mail:</p>
-        <a target="_blank" class="underline text-2xl opacity-50 mb-7" href="mailto:hello@luma-production.com">hello@luma-production.com</a>
-        <p class="text-4xl">Write a DM:</p>
-        <a target="_blank" class="underline text-2xl opacity-50 mb-7" href="https://www.instagram.com/luma.production_/">IG: @luma.production_</a>
-        <p class="text-4xl">Call:</p>
-        <a target="_blank" class="underline text-2xl opacity-50" href="tel:+420776781248">+420 776 781 248</a>
+<div class="bg-white text-center h-[100vh]">
+    <a href="/"><h1 class="text-[#1e1e1e] font-['Montserrat'] font-black text-6xl py-6 mb-[2vh] md:mb-[8vh]">Luma.</h1></a>
+    <div class="w-fulltext-center font-['Montaga'] text-3xl bg-white text-[#1e1e1e] flex flex-col gap-[0.5vh]">
+        <p class="text-4xl md:text-5xl">Write a mail:</p>
+        <a target="_blank" class="underline text-2xl md:text-3xl opacity-50 mb-7" href="mailto:hello@luma-production.com">hello@luma-production.com</a>
+        <p class="text-4xl md:text-5xl">Write a DM:</p>
+        <a target="_blank" class="underline text-2xl md:text-3xl opacity-50 mb-7" href="https://www.instagram.com/luma.production_/">IG: @luma.production_</a>
+        <p class="text-4xl md:text-txl">Call:</p>
+        <a target="_blank" class="underline text-2xl md:text-3xl opacity-50" href="tel:+420776781248">+420 776 781 248</a>
     </div>
-    <div class="bg-white text-[#1e1e1e] w-full px-[10%] md:px-[30%] flex flex-col justify-center items-center h-[30vh] italic pb-20 text-center">
-        <img src="/icons/quote.svg" width="50em" alt="quote" class="opacity-30 pb-3">
+    <div class="bg-white text-[#1e1e1e] w-full px-[10%] md:px-[30%] flex flex-col justify-center items-center h-[20vh] italic mt-[12vh]">
+        <img src="/icons/quote.svg" width="40vh" alt="quote" class="opacity-30 pb-3">
         <p class="opacity-50 pb-3">“Ideas are of themselves extraordinarily valuable, but an idea is just an idea. Almost anyone can think up an idea. The thing that counts is developing it into a practical product.”</p>
         <p class="opacity-40 text-sm"> - Henry Ford</p>
     </div>
