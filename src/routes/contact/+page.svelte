b3e15abbf52b7dc84fc94fa6922a93b5cfbb9574
Hodan <hodan>
2024-11-25 19:44:04 +0100

    fix: fix contact page responsiveness

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,6 @@
 <div class="bg-white text-center h-[100vh]">
     <a href="/"><h1 class="text-[#1e1e1e] font-['Montserrat'] font-black text-6xl py-6 mb-[2vh] md:mb-[8vh]">Luma.</h1></a>
-    <div class="w-fulltext-center font-['Montaga'] text-3xl bg-white text-[#1e1e1e] flex flex-col gap-[0.5vh]">
+    <div class="w-fulltext-center font-['Montaga'] text-3xl bg-white text-[#1e1e1e] flex flex-col gap-[0.5vh] md:gap-6">
         <p class="text-4xl md:text-5xl">Write a mail:</p>
         <a target="_blank" class="underline text-2xl md:text-3xl opacity-50 mb-7" href="mailto:hello@luma-production.com">hello@luma-production.com</a>
         <p class="text-4xl md:text-5xl">Write a DM:</p>
@@ -10,7 +10,7 @@
     </div>
     <div class="bg-white text-[#1e1e1e] w-full px-[10%] md:px-[30%] flex flex-col justify-center items-center h-[20vh] italic mt-[12vh]">
         <img src="/icons/quote.svg" width="40vh" alt="quote" class="opacity-30 pb-3">
-        <p class="opacity-50 pb-3">“Ideas are of themselves extraordinarily valuable, but an idea is just an idea. Almost anyone can think up an idea. The thing that counts is developing it into a practical product.”</p>
+        <p class="opacity-50 pb-3 md:text-lg">“Ideas are of themselves extraordinarily valuable, but an idea is just an idea. Almost anyone can think up an idea. The thing that counts is developing it into a practical product.”</p>
         <p class="opacity-40 text-sm"> - Henry Ford</p>
     </div>
 </div>
